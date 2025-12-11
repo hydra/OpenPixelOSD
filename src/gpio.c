36d9b93002b3ea347ea5bb912687a09c46e487d9
Dominic Clifton <me@dominicclifton.name>
2025-12-11 18:07:03 +0100

    Add a blinky variant, so that MCU operation can be observed.

--- src/gpio.c
+++ src/gpio.c
@@ -8,18 +8,31 @@ void gpio_init(void)
 {
     LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    /* GPIO Ports Clock Enable */
+    /* GPIO Ports Clock Enable (for LED) */
     LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
+
+    // LED on by default, so that correct flashing and the boot process can be observed.
+    LL_GPIO_SetOutputPin(LED_STATE_GPIO_Port, LED_STATE_Pin);
+
+    /**/
+    GPIO_InitStruct.Pin = LED_STATE_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(LED_STATE_GPIO_Port, &GPIO_InitStruct);
+
+#if defined(BUILD_VARIANT_BLINKY)
+    return;
+#endif
+
+    /* GPIO Ports Clock Enable */
     LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOF);
     LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
     LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
 
     /**/
     LL_GPIO_ResetOutputPin(SPI2_CS_GPIO_Port, SPI2_CS_Pin);
-
-    /**/
-    // LED on by default, so that correct flashing and the boot process can be observed.
-    LL_GPIO_SetOutputPin(LED_STATE_GPIO_Port, LED_STATE_Pin);
 
     /**/
     GPIO_InitStruct.Pin = USER_KEY_Pin;
@@ -48,14 +61,6 @@ void gpio_init(void)
     GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
     GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
     LL_GPIO_Init(SPI2_SCK_GPIO_Port, &GPIO_InitStruct);
-
-    /**/
-    GPIO_InitStruct.Pin = LED_STATE_Pin;
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-    LL_GPIO_Init(LED_STATE_GPIO_Port, &GPIO_InitStruct);
 
     /**/
     GPIO_InitStruct.Pin = BOOT_KEY_Pin;
